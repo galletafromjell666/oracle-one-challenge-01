d06dfd94df97a3c3c092e7014207af30363102a8
galletafromjell666 <giococu666@gmail.com>
2022-08-08 22:08:25 -0600

    finished crypt logic

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ console.log("Hello there");
 
 const btnEncriptar = document.querySelector(".btn.encriptar");
 const btnDesencriptar = document.querySelector(".btn.desencriptar");
+const loader = document.querySelector(".loader");
 const textArea = document.getElementById("textarea-input");
-//console.table({btnEncriptar, btnDesencriptar})
+const outputDiv = document.querySelector(".output");
+const textOutput = document.getElementById("text-output");
 
+const reset = () => {
+  loader.classList.add("hidden")
+  outputDiv.style.alignItems = "stretch";
+  textOutput.removeChild(textOutput.firstElementChild);
+  const h2element = document.createElement("h2");
+  return { h2element };
+};
 const encriptar = () => {
+  const { h2element } = reset();
   let { value } = textArea;
   let response = value
     .replaceAll("e", "enter")
@@ -14,9 +24,12 @@ const encriptar = () => {
     .replaceAll("o", "ober")
     .replaceAll("u", "ufat");
   console.log(`encriptar response =\n ${response}`);
+  h2element.textContent = response;
+  textOutput.appendChild(h2element);
 };
 
 const desencriptar = () => {
+  const { h2element } = reset();
   let { value } = textArea;
   let response = value
     .replaceAll("enter", "e")
@@ -25,6 +38,8 @@ const desencriptar = () => {
     .replaceAll("ober", "o")
     .replaceAll("ufat", "u");
   console.log(`desencriptar response =\n ${response}`);
+  h2element.textContent = response;
+  textOutput.appendChild(h2element);
 };
 btnEncriptar.addEventListener("click", encriptar);
 btnDesencriptar.addEventListener("click", desencriptar);
